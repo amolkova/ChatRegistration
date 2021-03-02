d19c1724d736c7f30a31c304a23d925ec59d051c
amolkova <am7171@mail.ru>
2021-03-02 18:48:50 +0300

    init commit

---
+++ server/src/main/java/server/ClientHandler.java
@@ -0,0 +1,140 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import commands.Command;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private int maxTime;
+    private String nickname;
+    private String login;
+    
+    public ClientHandler(Server server, Socket socket) {
+        maxTime = 5000;
+        try {
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            
+            new Thread(() -> {
+                try {
+                    
+                    // setting socket timeout
+                    socket.setSoTimeout(maxTime);
+                    
+                    // authentication cycle
+                    while (true) {
+                        String str = in.readUTF();
+                        
+                        // if the command disconnects
+                        if (str.equals(Command.END)) {
+                            out.writeUTF(Command.END);
+                            throw new RuntimeException("the client wants to disconnect");
+                        }
+                        
+                        // if command authentication
+                        if (str.startsWith(Command.AUTH)) {
+                            String[] token = str.split("\\s", 3);
+                            if (token.length < 3) {
+                                continue;
+                            }
+                            String newNick = server.getAuthService().getNicknameByLoginAndPassword(token[1], token[2]);
+                            login = token[1];
+                            if (newNick != null) {
+                                if (!server.isLoginAuthenticated(login)) {
+                                    nickname = newNick;
+                                    sendMsg(Command.AUTH_OK + " " + nickname);
+                                    server.subscribe(this);
+                                    System.out.println("client: " + socket.getRemoteSocketAddress()
+                                        + " connected with nick: " + nickname);
+                                    break;
+                                } else {
+                                    sendMsg("this account is already in use");
+                                }
+                            } else {
+                                sendMsg("wrong username / password ");
+                            }
+                        }
+                        
+                        // if command registration
+                        if (str.startsWith(Command.REG)) {
+                            String[] token = str.split("\\s", 4);
+                            if (token.length < 4) {
+                                continue;
+                            }
+                            boolean regSuccess = server.getAuthService().registration(token[1], token[2], token[3]);
+                            if (regSuccess) {
+                                sendMsg(Command.REG_OK);
+                            } else {
+                                sendMsg(Command.REG_NO);
+                            }
+                        }
+                        
+                    }
+                    // Working case
+                    while (true) {
+                        String str = in.readUTF();
+                        
+                        if (str.startsWith("/")) {
+                            if (str.equals(Command.END)) {
+                                out.writeUTF(Command.END);
+                                break;
+                            }
+                            
+                            if (str.startsWith(Command.PRIVATE_MSG)) {
+                                String[] token = str.split("\\s", 3);
+                                if (token.length < 3) {
+                                    continue;
+                                }
+                                server.privateMsg(this, token[1], token[2]);
+                            }
+                        } else {
+                            server.broadcastMsg(this, str);
+                        }
+                    }
+                    // SocketTimeoutException
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Error: The waiting time is over " + maxTime);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    server.unsubscribe(this);
+                    System.out.println("Client disconnected: " + nickname);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String getNickname() {
+        return nickname;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+}
